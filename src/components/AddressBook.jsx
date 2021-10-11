31e36e0a4f8fa80833fef8f5038f76ad13b3c8bc
barrard <barrard@gmail.com>
2021-10-11 00:50:46 -0700

    added context

--- src/components/AddressBook.jsx
+++ src/components/AddressBook.jsx
@@ -1,14 +1,31 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Context from "../Context";
 import { FullScreenContainer } from "./styled";
 import SideBar from "./SideBar";
 import Details from "./Details";
 
+const blankContact = { id: 0, fName: "", lName: "", emails: [] };
 export default function AddressBook() {
+	const [contacts, setContacts] = useState([]);
+	const [selectedContact, setSelectedContact] = useState(blankContact);
+
+	const saveContact = () => {
+		console.log(selectedContact);
+	};
+
 	return (
-		<FullScreenContainer>
-			<SideBar />
-			<Details />
-		</FullScreenContainer>
+		<Context.Provider
+			value={{
+				contacts,
+				selectedContact,
+				setSelectedContact,
+				saveContact,
+			}}
+		>
+			<FullScreenContainer>
+				<SideBar />
+				<Details />
+			</FullScreenContainer>
+		</Context.Provider>
 	);
 }
