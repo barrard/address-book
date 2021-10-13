663a9f51b12b690f2a5eb3a41b66e9fc0f6047e6
David Barrar <barrard@gmail.com>
2021-10-13 13:05:41 -0700

    remove console.logs

--- src/components/AddressBook.jsx
+++ src/components/AddressBook.jsx
@@ -6,149 +6,148 @@ import Details from "./Details";
 import uuid from "react-uuid";
 
 const blankContact = () => ({
-	id: uuid(),
-	fName: "",
-	lName: "",
-	emails: [],
-	saved: false,
+  id: uuid(),
+  fName: "",
+  lName: "",
+  emails: [],
+  saved: false,
 });
 export default function AddressBook() {
-	const [edits, setEdits] = useState(false);
-	const [addNew, setAddNew] = useState(false);
-	const [contacts, setContacts] = useState({});
-	const [filterContacts, setFilterContacts] = useState({});
-	const [selectedContact, setSelectedContact] = useState(blankContact());
-	const [emailEdits, setEmailEdits] = useState(false);
+  const [edits, setEdits] = useState(false);
+  const [addNew, setAddNew] = useState(false);
+  const [contacts, setContacts] = useState({});
+  const [filterContacts, setFilterContacts] = useState({});
+  const [selectedContact, setSelectedContact] = useState(blankContact());
+  const [emailEdits, setEmailEdits] = useState(false);
 
-	//Details form state
-	const [id, setId] = useState(selectedContact.id);
-	const [fName, setFName] = useState(selectedContact.fName);
-	const [lName, setLName] = useState(selectedContact.lName);
-	const [emails, setEmails] = useState(selectedContact.emails);
-	const [newEmail, setNewEmail] = useState("");
-	const [search, setSearch] = useState("");
+  //Details form state
+  const [id, setId] = useState(selectedContact.id);
+  const [fName, setFName] = useState(selectedContact.fName);
+  const [lName, setLName] = useState(selectedContact.lName);
+  const [emails, setEmails] = useState(selectedContact.emails);
+  const [newEmail, setNewEmail] = useState("");
+  const [search, setSearch] = useState("");
 
-	useEffect(() => {
-		//get from local store
-		let contacts = JSON.parse(localStorage.getItem("contacts"));
-		setContacts({ ...contacts });
-		setFilterContacts({ ...contacts });
-	}, []);
+  useEffect(() => {
+    //get from local store
+    let contacts = JSON.parse(localStorage.getItem("contacts"));
+    setContacts({ ...contacts });
+    setFilterContacts({ ...contacts });
+  }, []);
 
-	//Search events
-	useEffect(() => {
-		console.log(search);
-		let foundContacts = {};
+  //Search events
+  useEffect(() => {
+    let foundContacts = {};
 
-		Object.entries(contacts).forEach(([id, contact]) => {
-			let { fName, lName, emails } = contact;
-			//find names that start with search
-			if (fName.toLowerCase().startsWith(search)) {
-				foundContacts[id] = contact;
-			}
-			if (lName.toLowerCase().startsWith(search)) {
-				foundContacts[id] = contact;
-			}
+    Object.entries(contacts).forEach(([id, contact]) => {
+      let { fName, lName, emails } = contact;
+      //find names that start with search
+      if (fName.toLowerCase().startsWith(search)) {
+        foundContacts[id] = contact;
+      }
+      if (lName.toLowerCase().startsWith(search)) {
+        foundContacts[id] = contact;
+      }
 
-			//find emails that include
-			emails.forEach((email) => {
-				if (email.toLowerCase().includes(search)) {
-					foundContacts[id] = contact;
-				}
-			});
-		});
-		setFilterContacts(foundContacts);
-	}, [search, contacts]);
+      //find emails that include
+      emails.forEach((email) => {
+        if (email.toLowerCase().includes(search)) {
+          foundContacts[id] = contact;
+        }
+      });
+    });
+    setFilterContacts(foundContacts);
+  }, [search, contacts]);
 
-	// sets local state whenever selectedContact updates
-	useEffect(() => {
-		setFName(selectedContact.fName);
-		setLName(selectedContact.lName);
-		setEmails([...selectedContact.emails]);
-		setId(selectedContact.id);
-	}, [selectedContact]);
+  // sets local state whenever selectedContact updates
+  useEffect(() => {
+    setFName(selectedContact.fName);
+    setLName(selectedContact.lName);
+    setEmails([...selectedContact.emails]);
+    setId(selectedContact.id);
+  }, [selectedContact]);
 
-	useEffect(() => {
-		if (selectedContact.edits) return;
-		setSelectedContact({ ...selectedContact, edits: true });
-	}, [emails, fName, lName, selectedContact]);
+  useEffect(() => {
+    if (selectedContact.edits) return;
+    setSelectedContact({ ...selectedContact, edits: true });
+  }, [emails, fName, lName, selectedContact]);
 
-	const validateContact = (contact) => {
-		let _fName = fName.trim();
-		let _lName = lName.trim();
-		if (!_fName) return alert("Contact needs a First Name");
-		if (!_lName) return alert("Contact needs a Last Name");
-		return true;
-	};
+  const validateContact = (contact) => {
+    let _fName = fName.trim();
+    let _lName = lName.trim();
+    if (!_fName) return alert("Contact needs a First Name");
+    if (!_lName) return alert("Contact needs a Last Name");
+    return true;
+  };
 
-	const deleteContact = (id) => {
-		if (
-			window.confirm(
-				`Are you sure you want to delete ${selectedContact.fName} ${selectedContact.lName} from your contacts?`
-			)
-		) {
-			delete contacts[selectedContact.id];
-			setContacts({ ...contacts });
-			setSelectedContact(blankContact());
-		}
-	};
+  const deleteContact = (id) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete ${selectedContact.fName} ${selectedContact.lName} from your contacts?`
+      )
+    ) {
+      delete contacts[selectedContact.id];
+      setContacts({ ...contacts });
+      setSelectedContact(blankContact());
+    }
+  };
 
-	const saveContact = (contact) => {
-		if (!validateContact(contact)) return;
-		//validate
-		contacts[id] = {
-			id,
-			fName: fName.trim(),
-			lName: lName.trim(),
-			emails,
-			saved: true,
-			edits: false,
-		};
+  const saveContact = (contact) => {
+    if (!validateContact(contact)) return;
+    //validate
+    contacts[id] = {
+      id,
+      fName: fName.trim(),
+      lName: lName.trim(),
+      emails,
+      saved: true,
+      edits: false,
+    };
 
-		setSelectedContact({ ...contacts[id] });
-		setContacts({ ...contacts });
-		setAddNew(false);
-		setEdits(false);
-		localStorage.setItem("contacts", JSON.stringify(contacts));
-		alert(`Saved contact ${fName} ${lName}`);
-	};
+    setSelectedContact({ ...contacts[id] });
+    setContacts({ ...contacts });
+    setAddNew(false);
+    setEdits(false);
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+    alert(`Saved contact ${fName} ${lName}`);
+  };
 
-	const createNewContact = () => {
-		setSelectedContact(blankContact());
-	};
+  const createNewContact = () => {
+    setSelectedContact(blankContact());
+  };
 
-	return (
-		<Context.Provider
-			value={{
-				addNew,
-				contacts,
-				createNewContact,
-				deleteContact,
-				edits,
-				emails,
-				emailEdits,
-				filterContacts,
-				fName,
-				lName,
-				newEmail,
-				selectedContact,
-				saveContact,
-				search,
-				setAddNew,
-				setEdits,
-				setEmailEdits,
-				setEmails,
-				setFName,
-				setLName,
-				setNewEmail,
-				setSearch,
-				setSelectedContact,
-			}}
-		>
-			<FullScreenContainer>
-				<SideBar />
-				<Details />
-			</FullScreenContainer>
-		</Context.Provider>
-	);
+  return (
+    <Context.Provider
+      value={{
+        addNew,
+        contacts,
+        createNewContact,
+        deleteContact,
+        edits,
+        emails,
+        emailEdits,
+        filterContacts,
+        fName,
+        lName,
+        newEmail,
+        selectedContact,
+        saveContact,
+        search,
+        setAddNew,
+        setEdits,
+        setEmailEdits,
+        setEmails,
+        setFName,
+        setLName,
+        setNewEmail,
+        setSearch,
+        setSelectedContact,
+      }}
+    >
+      <FullScreenContainer>
+        <SideBar />
+        <Details />
+      </FullScreenContainer>
+    </Context.Provider>
+  );
 }
--- src/components/SaveDetails.jsx
+++ src/components/SaveDetails.jsx
@@ -5,57 +5,49 @@ import Context from "../Context";
 
 import sc from "../styles-config";
 export default function SaveDetails() {
-	let {
-		saveContact,
-		setSelectedContact,
-		selectedContact,
-		deleteContact,
-		setNewEmail,
-		edits,
-	} = useContext(Context);
-	console.log(selectedContact.edits);
-	return (
-		<ButtonsContainer>
-			{selectedContact.edits && (
-				<>
-					{/* SAVE */}
-					<Button
-						style={{
-							boxShadow: edits ? "0px 0px 10px 2px red" : "none",
-						}}
-						onClick={saveContact}
-						bgColor={sc["primary"]}
-						color={sc["white"]}
-						text="Save Contact"
-					/>
+  let { saveContact, setSelectedContact, selectedContact, deleteContact, setNewEmail, edits } = useContext(Context);
+  return (
+    <ButtonsContainer>
+      {selectedContact.edits && (
+        <>
+          {/* SAVE */}
+          <Button
+            style={{
+              boxShadow: edits ? "0px 0px 10px 2px red" : "none",
+            }}
+            onClick={saveContact}
+            bgColor={sc["primary"]}
+            color={sc["white"]}
+            text="Save Contact"
+          />
 
-					{/* CANCEL */}
-					<Button
-						style={{ marginLeft: "2em" }}
-						onClick={() => {
-							setSelectedContact({
-								...selectedContact,
-								edits: false,
-							});
-							setNewEmail("");
-						}}
-						color={sc["primary"]}
-						bgColor={sc["white"]}
-						text="Cancel"
-					/>
-				</>
-			)}
+          {/* CANCEL */}
+          <Button
+            style={{ marginLeft: "2em" }}
+            onClick={() => {
+              setSelectedContact({
+                ...selectedContact,
+                edits: false,
+              });
+              setNewEmail("");
+            }}
+            color={sc["primary"]}
+            bgColor={sc["white"]}
+            text="Cancel"
+          />
+        </>
+      )}
 
-			{/* DELETE */}
-			{selectedContact.saved && (
-				<Button
-					onClick={deleteContact}
-					style={{ position: "absolute", right: "2em" }}
-					color="red"
-					bgColor={sc["white"]}
-					text="Delete Contact"
-				/>
-			)}
-		</ButtonsContainer>
-	);
+      {/* DELETE */}
+      {selectedContact.saved && (
+        <Button
+          onClick={deleteContact}
+          style={{ position: "absolute", right: "2em" }}
+          color="red"
+          bgColor={sc["white"]}
+          text="Delete Contact"
+        />
+      )}
+    </ButtonsContainer>
+  );
 }
