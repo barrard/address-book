55aa705d29f6194e2a9de684299c598711561c8e
barrard <barrard@gmail.com>
2021-10-17 21:53:09 -0700

    fixed scroll bar when no contacts listed

--- src/components/AddressBook/ContactsList.jsx
+++ src/components/AddressBook/ContactsList.jsx
@@ -9,25 +9,30 @@ export default function ContactsList() {
 	let { filterContacts } = useContext(Context);
 
 	return (
-		<ListContainer>
-			<div style={{ height: "100%" }} className="scrollbar-shadow">
-				<Scrollable.Shadow>
-					<Scrollable>
-						{Object.keys(filterContacts).length === 0 && (
-							<Text style={{ paddingLeft: "2em" }}>
-								No Contacts
-							</Text>
-						)}
-						{Object.keys(filterContacts).length > 0 &&
-							Object.keys(filterContacts).map((id) => (
-								<Contact
-									contact={filterContacts[id]}
-									key={id}
-								/>
-							))}
-					</Scrollable>
-				</Scrollable.Shadow>
-			</div>
-		</ListContainer>
+		<>
+			{Object.keys(filterContacts).length === 0 && (
+				<Text style={{ paddingLeft: "2em" }}>No Contacts</Text>
+			)}
+			{Object.keys(filterContacts).length > 0 && (
+				<ListContainer>
+					<div
+						style={{ height: "100%" }}
+						className="scrollbar-shadow"
+					>
+						<Scrollable.Shadow>
+							<Scrollable>
+								{Object.keys(filterContacts).length > 0 &&
+									Object.keys(filterContacts).map((id) => (
+										<Contact
+											contact={filterContacts[id]}
+											key={id}
+										/>
+									))}
+							</Scrollable>
+						</Scrollable.Shadow>
+					</div>
+				</ListContainer>
+			)}
+		</>
 	);
 }
